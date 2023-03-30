daebd3e5809a25a5300f2b72f94c78d0031db722
Paweł Brewczyński <brewczynskipaul@gmail.com>
2023-03-30 17:48:50 +0200

    Initial Commit

---
+++ PassingClosure_RetainCycle_or_not/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  PassingClosure_RetainCycle_or_not
+//
+//  Created by pbrewczynski on 30/03/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PassingClosure_RetainCycle_or_not/PassingClosure_RetainCycle_or_notApp.swift
@@ -0,0 +1,17 @@
+//
+//  PassingClosure_RetainCycle_or_notApp.swift
+//  PassingClosure_RetainCycle_or_not
+//
+//  Created by pbrewczynski on 30/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct PassingClosure_RetainCycle_or_notApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
